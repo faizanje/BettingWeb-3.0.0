245d879dad313b60e5a75d4d1caa5b06e68f99d6
Faizan Malik <malickfaizann@gmail.com>
2023-05-01 05:38:03 -0700

    Fixed done

--- assets/js/4d-bet/simple-betting-1.js
+++ assets/js/4d-bet/simple-betting-1.js
@@ -3,3 +3,41 @@ $(function () {
     populateSelectedBrandLogosProgrammatically();
     populateBetTitle();
 })
+
+function handleNextSimpleBetting4d() {
+
+    // Define an object to store the selected brand logos and their selected chips
+    const selectedBrandLogos = {};
+
+// Get all brand logos with the 'active' class
+    $('.brand_logo .icon_box.active').each(function() {
+        var logo = $(this).find('.logo_2').attr('src');
+        var chips = $(this).parent().next('.chips').find('.chip.selected').map(function() {
+            return $(this).text();
+        }).get();
+
+        // Add the selected brand logo and its selected chips to the object
+        selectedBrandLogos[logo] = chips;
+    });
+
+// Log the selected brand logos and their selected chips to the console
+    console.log('selectedBrandLogos',selectedBrandLogos);
+
+    const isSelectedBrandsEmpty = Object.keys(selectedBrandLogos).length === 0
+    if (isSelectedBrandsEmpty) {
+        $(".modal").addClass('active')
+        return
+    }
+
+    // console.log(JSON.stringify(selectedBrandLogos))
+    saveSelectedBrandLogos(selectedBrandLogos);
+    // console.log(window.location)
+    // console.log(document.location)
+    window.open('simple-betting-2.html', '_self');
+
+
+
+
+    // window.location.assign("/simple-betting-2.html");
+    // window.location.assign("/simple-betting-2.html");
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -299,36 +299,6 @@ function saveSelectedBrandLogos(selectedBrandLogos) {
     window.sessionStorage.setItem("selectedBrandLogos", JSON.stringify(selectedBrandLogos))
 }
 
-function handleNextSimpleBetting42rd() {
-
-    // Define an object to store the selected brand logos and their selected chips
-    const selectedBrandLogos = {};
-
-// Get all brand logos with the 'active' class
-    $('.brand_logo .icon_box.active').each(function() {
-        var logo = $(this).find('.logo_2').attr('src');
-        var chips = $(this).parent().next('.chips').find('.chip.selected').map(function() {
-            return $(this).text();
-        }).get();
-
-        // Add the selected brand logo and its selected chips to the object
-        selectedBrandLogos[logo] = chips;
-    });
-
-// Log the selected brand logos and their selected chips to the console
-    console.log('selectedBrandLogos',selectedBrandLogos);
-
-    console.log(JSON.stringify(selectedBrandLogos))
-    saveSelectedBrandLogos(selectedBrandLogos);
-    console.log(window.location)
-    console.log(document.location)
-    window.open('simple-betting-2.html', '_self');
-
-
-
-    // window.location.assign("/simple-betting-2.html");
-    // window.location.assign("/simple-betting-2.html");
-}
 
 function appendFastBettingDiv() {
     const fastBettingContainer = $(".fast-betting-container");
